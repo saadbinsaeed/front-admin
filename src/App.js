d5cd9207d17fc6d359229ff69bd7440cc8c3c991
Saad Bin Saeed <saad.saeed@mi-c3.com>
2018-04-09 22:23:37 +0500

    antd

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
+// @flow
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Button from 'antd/lib/button';
+import SignUpFrom from './components/SignUpFrom';
 import './App.css';
+import { Row, Col } from 'antd'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Row>
+             <Col offset={4} span={16}>
+                <SignUpFrom />
+             </Col>
+         </Row>
       </div>
     );
   }
